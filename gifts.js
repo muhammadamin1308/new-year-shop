34fd44d85c74e25231fd0f6a7dc35f30929551d0
Muhammadamin <79371764+muhammadamin1308@users.noreply.github.com>
2025-05-08 21:45:19 +0500

    feat: add modal, to top btn. fix: mobile view

--- gifts.js
+++ gifts.js
@@ -16,78 +16,11 @@ menu.addEventListener("click", (e) => {
   }
 });
 
-// // ALL GIFTS
-// async function allGifts() {
-//     const url = "../gifts.json";
-//     const allGiftContainer = document.querySelector("#all-gifts");
-//     try {
-//       const res = await fetch(url);
-//       if (!res.ok) {
-//         throw new Error(`Failed to fetch JSON: ${res.statusText}`);
-//       }
-//       const data = await res.json();
-//       renderAllGifts(data, allGiftContainer);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
-
-//   function renderAllGifts(allGifts, container) {
-//     const allGiftHTML = allGifts
-//       .map((gift) => {
-//         return `<div class="column-3 col">
-//       <div class="img">
-//         <img src="../images/gift-${gift.category
-//           .split(" ")
-//           .join("-")
-//           .toLowerCase()}.png" alt="${gift.name}" />
-//       </div>
-//       <div class="description">
-//         <h4 class="h-4" style="color: #4361ff">${gift.category}</h4>
-//         <h3 class="h-3">${gift.name}</h3>
-//       </div>
-//     </div>`;
-//       })
-//       .join("");
-//     container.innerHTML = allGiftHTML;
-//   }
-
-//   allGifts();
-
-// CATEGORY GIFTS
-// const categories = document.querySelectorAll(".category-name"); // Use plural for clarity
-
-// categories.forEach(category => {  // Iterate over each category element
-//     category.addEventListener("click", (event) => {  // Correct event handling
-//         let output = "";
-//         const categoryName = getCategorynName(event.target.textContent); // Use textContent and target
-
-//         console.log(categoryName);
-
-//         if(categoryName == 'For Work'){
-//             allGifts.apply
-//         }
-//     });
-// });
-
-// function getCategorynName(categoryName) {
-//     switch (categoryName) {
-//         case "For Work":
-//         case "For Health":
-//         case "For Harmony":
-//             // Return the category name; no need for separate output variable
-//             return categoryName;
-//         default:
-//             return "Unknown Category"; // Handle cases where the category isn't recognized
-//     }
-// }
-
 // Fetch and render categories
 async function loadCategories() {
   try {
     const response = await fetch("../gifts.json");
     const categories = await response.json();
-
     renderCategoryCards(categories);
     displayAllItems(categories);
   } catch (error) {
@@ -141,6 +74,7 @@ function displayCategoryItems(items) {
   itemsContainer.innerHTML = "";
 
   itemsContainer.innerHTML = items.map((item) => createCard(item)).join("");
+  attachCardClickListeners(items);
 }
 
 function displayAllItems(categories) {
@@ -149,20 +83,17 @@ function displayAllItems(categories) {
 
   const allCards = categories.map((item) => createCard(item)).join("");
   itemsContainer.innerHTML = allCards;
+  attachCardClickListeners(categories);
 }
 
 function createCard(item) {
-  const superpowers = Object.entries(item.superpowers)
-    .map(([key, value]) => `<li><strong>${key}:</strong> ${value}</li>`)
-    .join("");
-
   return `
-      <div class="column-3 col">
+      <div class="column-3 col" id="${item.id}">
         <div class="img">
           <img src="../images/gift-${item.category
-            .split(" ")
-            .join("-")
-            .toLowerCase()}.png" alt="${item.name}" />
+      .split(" ")
+      .join("-")
+      .toLowerCase()}.png" alt="${item.name}" />
         </div>
         <div class="description">
           <h4 class="h-4" style="color: #4361ff">${item.category}</h4>
@@ -173,23 +104,128 @@ function createCard(item) {
 
 loadCategories();
 
+
+let cardId
+
+function attachCardClickListeners(gifts) {
+  document.querySelectorAll(".column-3").forEach((card) => {
+    card.addEventListener("click", () => {
+      cardId = parseInt(card.getAttribute("id"), 10);
+      displayModal(gifts);
+      document.body.classList.toggle('no-scroll');
+      const modalCont = document.getElementById("modal-cont");
+      if (modalCont) {
+        modalCont.addEventListener("click", (e) => {
+          if (e.target === modalCont) {
+            modalCont.style.display = "none";
+            document.body.classList.remove('no-scroll');
+          }
+        });
+      }
+    });
+  });
+}
+
+displayModal = (gifts) => {
+  const modalCont = document.getElementById("modal-container");
+  const item = gifts[cardId - 1];
+  modalCont.innerHTML = createModal(item);
+};
+
+function createModal(item) {
+  if (!item) {
+    console.error("Invalid item or missing 'superpowers' property:", item);
+    return `<div class="modal-error">Item data is invalid.</div>`;
+  }
+
+  const superpowers = Object.entries(item.superpowers)
+    .map(([key, value]) => `<li class="superpower-item">
+                <p class="paragraph" style="padding: 0;">${key}</p>
+                <div class="stars-cont">
+                  <p class="paragraph stars-text">${value.slice(1, 4)}</p>
+                  <div class="stars">
+                    <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M11.1959 9.88162L10.6482 9.56542L12.1158 9.17219L11.8732 8.26704L9.50055 8.90278L8.38146 8.25667C8.39689 8.17336 8.40538 8.08765 8.40538 7.99997C8.40538 7.91229 8.39692 7.82655 8.38146 7.74327L9.50055 7.09716L11.8732 7.7329L12.1158 6.82775L10.6482 6.43452L11.1959 6.11831L13.546 5.97725L13.8921 4.02063L12.0246 3.34203L10.7274 5.30677L10.1797 5.62297L10.5729 4.15545L9.66778 3.91293L9.03204 6.28561L7.91226 6.93211C7.78247 6.82103 7.63242 6.73313 7.4683 6.67494V5.3828L9.20521 3.64586L8.5426 2.98325L7.46827 4.05755V3.42515L8.51792 1.32584L6.99976 0L5.48157 1.3259L6.53122 3.42521V4.05761L5.45689 2.98332L4.79429 3.64592L6.53119 5.38286V6.675C6.36708 6.73319 6.21702 6.82109 6.08724 6.93217L4.96746 6.28568L4.33171 3.91299L3.42656 4.15551L3.81979 5.62304L3.27213 5.30684L1.9749 3.34209L0.107422 4.02069L0.453485 5.97731L2.80362 6.11838L3.35128 6.43458L1.88375 6.82781L2.1263 7.73296L4.49898 7.09722L5.61807 7.74333C5.60264 7.82664 5.59414 7.91235 5.59414 8.00003C5.59414 8.08771 5.60261 8.17345 5.61807 8.25673L4.49898 8.90285L2.1263 8.2671L1.88375 9.17226L3.35128 9.56548L2.80362 9.88169L0.453485 10.0227L0.107422 11.9793L1.97493 12.6579L3.27216 10.6932L3.81985 10.377L3.42662 11.8445L4.33177 12.087L4.96752 9.71435L6.0873 9.06786C6.21708 9.17894 6.36714 9.26684 6.53125 9.32503V10.6172L4.79435 12.3541L5.45696 13.0167L6.53129 11.9424V12.5748L5.48163 14.6741L6.99983 16L8.51802 14.6741L7.46837 12.5748V11.9424L8.5427 13.0167L9.2053 12.3541L7.4684 10.6172V9.32503C7.63251 9.26684 7.78257 9.17894 7.91235 9.06786L9.03213 9.71435L9.66788 12.087L10.573 11.8445L10.1798 10.377L10.7275 10.6932L12.0247 12.6579L13.8922 11.9793L13.5462 10.0227L11.1959 9.88162Z"
+                      fill="#FF4646" />
+                    </svg>
+                    <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M11.1959 9.88162L10.6482 9.56542L12.1158 9.17219L11.8732 8.26704L9.50055 8.90278L8.38146 8.25667C8.39689 8.17336 8.40538 8.08765 8.40538 7.99997C8.40538 7.91229 8.39692 7.82655 8.38146 7.74327L9.50055 7.09716L11.8732 7.7329L12.1158 6.82775L10.6482 6.43452L11.1959 6.11831L13.546 5.97725L13.8921 4.02063L12.0246 3.34203L10.7274 5.30677L10.1797 5.62297L10.5729 4.15545L9.66778 3.91293L9.03204 6.28561L7.91226 6.93211C7.78247 6.82103 7.63242 6.73313 7.4683 6.67494V5.3828L9.20521 3.64586L8.5426 2.98325L7.46827 4.05755V3.42515L8.51792 1.32584L6.99976 0L5.48157 1.3259L6.53122 3.42521V4.05761L5.45689 2.98332L4.79429 3.64592L6.53119 5.38286V6.675C6.36708 6.73319 6.21702 6.82109 6.08724 6.93217L4.96746 6.28568L4.33171 3.91299L3.42656 4.15551L3.81979 5.62304L3.27213 5.30684L1.9749 3.34209L0.107422 4.02069L0.453485 5.97731L2.80362 6.11838L3.35128 6.43458L1.88375 6.82781L2.1263 7.73296L4.49898 7.09722L5.61807 7.74333C5.60264 7.82664 5.59414 7.91235 5.59414 8.00003C5.59414 8.08771 5.60261 8.17345 5.61807 8.25673L4.49898 8.90285L2.1263 8.2671L1.88375 9.17226L3.35128 9.56548L2.80362 9.88169L0.453485 10.0227L0.107422 11.9793L1.97493 12.6579L3.27216 10.6932L3.81985 10.377L3.42662 11.8445L4.33177 12.087L4.96752 9.71435L6.0873 9.06786C6.21708 9.17894 6.36714 9.26684 6.53125 9.32503V10.6172L4.79435 12.3541L5.45696 13.0167L6.53129 11.9424V12.5748L5.48163 14.6741L6.99983 16L8.51802 14.6741L7.46837 12.5748V11.9424L8.5427 13.0167L9.2053 12.3541L7.4684 10.6172V9.32503C7.63251 9.26684 7.78257 9.17894 7.91235 9.06786L9.03213 9.71435L9.66788 12.087L10.573 11.8445L10.1798 10.377L10.7275 10.6932L12.0247 12.6579L13.8922 11.9793L13.5462 10.0227L11.1959 9.88162Z"
+                      fill="#FF4646" />
+                    </svg>
+                    <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M11.1959 9.88162L10.6482 9.56542L12.1158 9.17219L11.8732 8.26704L9.50055 8.90278L8.38146 8.25667C8.39689 8.17336 8.40538 8.08765 8.40538 7.99997C8.40538 7.91229 8.39692 7.82655 8.38146 7.74327L9.50055 7.09716L11.8732 7.7329L12.1158 6.82775L10.6482 6.43452L11.1959 6.11831L13.546 5.97725L13.8921 4.02063L12.0246 3.34203L10.7274 5.30677L10.1797 5.62297L10.5729 4.15545L9.66778 3.91293L9.03204 6.28561L7.91226 6.93211C7.78247 6.82103 7.63242 6.73313 7.4683 6.67494V5.3828L9.20521 3.64586L8.5426 2.98325L7.46827 4.05755V3.42515L8.51792 1.32584L6.99976 0L5.48157 1.3259L6.53122 3.42521V4.05761L5.45689 2.98332L4.79429 3.64592L6.53119 5.38286V6.675C6.36708 6.73319 6.21702 6.82109 6.08724 6.93217L4.96746 6.28568L4.33171 3.91299L3.42656 4.15551L3.81979 5.62304L3.27213 5.30684L1.9749 3.34209L0.107422 4.02069L0.453485 5.97731L2.80362 6.11838L3.35128 6.43458L1.88375 6.82781L2.1263 7.73296L4.49898 7.09722L5.61807 7.74333C5.60264 7.82664 5.59414 7.91235 5.59414 8.00003C5.59414 8.08771 5.60261 8.17345 5.61807 8.25673L4.49898 8.90285L2.1263 8.2671L1.88375 9.17226L3.35128 9.56548L2.80362 9.88169L0.453485 10.0227L0.107422 11.9793L1.97493 12.6579L3.27216 10.6932L3.81985 10.377L3.42662 11.8445L4.33177 12.087L4.96752 9.71435L6.0873 9.06786C6.21708 9.17894 6.36714 9.26684 6.53125 9.32503V10.6172L4.79435 12.3541L5.45696 13.0167L6.53129 11.9424V12.5748L5.48163 14.6741L6.99983 16L8.51802 14.6741L7.46837 12.5748V11.9424L8.5427 13.0167L9.2053 12.3541L7.4684 10.6172V9.32503C7.63251 9.26684 7.78257 9.17894 7.91235 9.06786L9.03213 9.71435L9.66788 12.087L10.573 11.8445L10.1798 10.377L10.7275 10.6932L12.0247 12.6579L13.8922 11.9793L13.5462 10.0227L11.1959 9.88162Z"
+                      fill="#FF4646" />
+                    </svg>
+                    <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M11.1959 9.88162L10.6482 9.56542L12.1158 9.17219L11.8732 8.26704L9.50055 8.90278L8.38146 8.25667C8.39689 8.17336 8.40538 8.08765 8.40538 7.99997C8.40538 7.91229 8.39692 7.82655 8.38146 7.74327L9.50055 7.09716L11.8732 7.7329L12.1158 6.82775L10.6482 6.43452L11.1959 6.11831L13.546 5.97725L13.8921 4.02063L12.0246 3.34203L10.7274 5.30677L10.1797 5.62297L10.5729 4.15545L9.66778 3.91293L9.03204 6.28561L7.91226 6.93211C7.78247 6.82103 7.63242 6.73313 7.4683 6.67494V5.3828L9.20521 3.64586L8.5426 2.98325L7.46827 4.05755V3.42515L8.51792 1.32584L6.99976 0L5.48157 1.3259L6.53122 3.42521V4.05761L5.45689 2.98332L4.79429 3.64592L6.53119 5.38286V6.675C6.36708 6.73319 6.21702 6.82109 6.08724 6.93217L4.96746 6.28568L4.33171 3.91299L3.42656 4.15551L3.81979 5.62304L3.27213 5.30684L1.9749 3.34209L0.107422 4.02069L0.453485 5.97731L2.80362 6.11838L3.35128 6.43458L1.88375 6.82781L2.1263 7.73296L4.49898 7.09722L5.61807 7.74333C5.60264 7.82664 5.59414 7.91235 5.59414 8.00003C5.59414 8.08771 5.60261 8.17345 5.61807 8.25673L4.49898 8.90285L2.1263 8.2671L1.88375 9.17226L3.35128 9.56548L2.80362 9.88169L0.453485 10.0227L0.107422 11.9793L1.97493 12.6579L3.27216 10.6932L3.81985 10.377L3.42662 11.8445L4.33177 12.087L4.96752 9.71435L6.0873 9.06786C6.21708 9.17894 6.36714 9.26684 6.53125 9.32503V10.6172L4.79435 12.3541L5.45696 13.0167L6.53129 11.9424V12.5748L5.48163 14.6741L6.99983 16L8.51802 14.6741L7.46837 12.5748V11.9424L8.5427 13.0167L9.2053 12.3541L7.4684 10.6172V9.32503C7.63251 9.26684 7.78257 9.17894 7.91235 9.06786L9.03213 9.71435L9.66788 12.087L10.573 11.8445L10.1798 10.377L10.7275 10.6932L12.0247 12.6579L13.8922 11.9793L13.5462 10.0227L11.1959 9.88162Z"
+                      fill="#FF4646" />
+                    </svg>
+                    <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M11.1959 9.88162L10.6482 9.56542L12.1158 9.17219L11.8732 8.26704L9.50055 8.90278L8.38146 8.25667C8.39689 8.17336 8.40538 8.08765 8.40538 7.99997C8.40538 7.91229 8.39692 7.82655 8.38146 7.74327L9.50055 7.09716L11.8732 7.7329L12.1158 6.82775L10.6482 6.43452L11.1959 6.11831L13.546 5.97725L13.8921 4.02063L12.0246 3.34203L10.7274 5.30677L10.1797 5.62297L10.5729 4.15545L9.66778 3.91293L9.03204 6.28561L7.91226 6.93211C7.78247 6.82103 7.63242 6.73313 7.4683 6.67494V5.3828L9.20521 3.64586L8.5426 2.98325L7.46827 4.05755V3.42515L8.51792 1.32584L6.99976 0L5.48157 1.3259L6.53122 3.42521V4.05761L5.45689 2.98332L4.79429 3.64592L6.53119 5.38286V6.675C6.36708 6.73319 6.21702 6.82109 6.08724 6.93217L4.96746 6.28568L4.33171 3.91299L3.42656 4.15551L3.81979 5.62304L3.27213 5.30684L1.9749 3.34209L0.107422 4.02069L0.453485 5.97731L2.80362 6.11838L3.35128 6.43458L1.88375 6.82781L2.1263 7.73296L4.49898 7.09722L5.61807 7.74333C5.60264 7.82664 5.59414 7.91235 5.59414 8.00003C5.59414 8.08771 5.60261 8.17345 5.61807 8.25673L4.49898 8.90285L2.1263 8.2671L1.88375 9.17226L3.35128 9.56548L2.80362 9.88169L0.453485 10.0227L0.107422 11.9793L1.97493 12.6579L3.27216 10.6932L3.81985 10.377L3.42662 11.8445L4.33177 12.087L4.96752 9.71435L6.0873 9.06786C6.21708 9.17894 6.36714 9.26684 6.53125 9.32503V10.6172L4.79435 12.3541L5.45696 13.0167L6.53129 11.9424V12.5748L5.48163 14.6741L6.99983 16L8.51802 14.6741L7.46837 12.5748V11.9424L8.5427 13.0167L9.2053 12.3541L7.4684 10.6172V9.32503C7.63251 9.26684 7.78257 9.17894 7.91235 9.06786L9.03213 9.71435L9.66788 12.087L10.573 11.8445L10.1798 10.377L10.7275 10.6932L12.0247 12.6579L13.8922 11.9793L13.5462 10.0227L11.1959 9.88162Z"
+                      fill="#FF4646" />
+                    </svg>
+                  </div>
+                </div>
+              </li>`)
+    .join("");
+
+  return `
+    <div class="modal-cont" id="modal-cont">    
+      <div class="modal">
+        <div class="img">
+          <img src="../images/gift-${item.category.split(" ").join("-").toLowerCase()}.png" alt="${item.name}" />
+        </div>
+        <div class="modal-desc">
+          <h4 class="h-4" style="color: #4361ff">${item.category}</h4>
+          <h3 class="h-3">${item.name}</h3>
+          <p class="desc paragraph">${item.description}</p>
+          <div class="superpowers">
+            <h4 class="h-4" style="color: black;">Adds superpowers to:</h4>
+            <ul class="superpowers-list">
+              ${superpowers}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>`;
+}
+
+// Add an event listener to close the modal when clicking anywhere in the modal container
+
+
 // TO TOP BUTTON
+
+const topBtnDiv = document.getElementById("top-btn");
+topBtnDiv.innerHTML = `      
+      <button class="btn arrow-top" id="scrollToTopButton">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 5V19" stroke="#FF4646" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M18 11L12 5" stroke="#FF4646" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M6 11L12 5" stroke="#FF4646" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+      </button>`
+
 const button = document.getElementById("scrollToTopButton");
 
-// Monitor scroll events
+
 window.addEventListener("scroll", () => {
-  // Check if user scrolled down 300px
   if (window.scrollY > 300) {
     button.style.display = "block";
   } else {
-    button.style.display = "none"; 
+    button.style.display = "none";
   }
 });
 
-// Add click event to scroll to top
+
 button.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
-    behavior: "smooth", // Smooth scrolling
+    behavior: "smooth",
   });
 });
